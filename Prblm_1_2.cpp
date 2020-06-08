d7eab8cdd07fc4a3141a9ebce2211c2a50f41474
Anjitha Nair <anjithanair.146@gmail.com>
2020-06-08 17:56:42 +0530

    all done

---
+++ Prblm_1_2.cpp
@@ -0,0 +1,25 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    int n;
+    cin>>n;
+    int len=n+(n-1);
+    for(int i=0;i<len;i++){
+         for(int j=0;j<len;j++){
+            int min=i<j?i:j;
+            min=min<len-j-1?min:len-j-1;
+            min=min<len-i-1?min:len-i-1;
+            cout<<n-min<<" ";
+
+        }
+        cout<<"\n";
+
+    }   
+    return 0;
+}
